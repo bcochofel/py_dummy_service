ee1f2926056047ae71ceffaa586d5e22791b78e8
Bruno Cochofel <bruno.cochofel@gmail.com>
2021-04-11 19:30:19 +0100

    feat: some logging messages

--- py_dummy_service/errors.py
+++ py_dummy_service/errors.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Blueprint, jsonify
 
 
@@ -6,9 +8,13 @@ bp = Blueprint("errors", __name__)
 
 @bp.app_errorhandler(404)
 def handle_404(err):
+    logging.debug("404")
+    logging.error("%s", err)
     return jsonify(error=str(err)), 404
 
 
 @bp.app_errorhandler(500)
 def handle_500(err):
+    logging.debug("500")
+    logging.error("%s", err)
     return jsonify(error=str(err)), 500
